80cdede10c2409c9537c936dd6cc062e227bfa5e
R3IS <101276269+xreiz0r@users.noreply.github.com>
2023-02-12 13:32:33 +0000

    Update DG.cpp

--- DG.cpp
+++ DG.cpp
@@ -74,7 +74,7 @@ map<char,int> /*controlcdotmoc/3a0608e5*/ example;
 	example.insert(std::make_pair('Q',17));
 	example.insert(std::make_pair('R',18));
 	example.insert(std::make_pair('S',19));
-	controlc.com/3a0608e5
+	//controlcdoot/3a0608e5
 
 	example.insert(std::make_pair('T',20));
 	
